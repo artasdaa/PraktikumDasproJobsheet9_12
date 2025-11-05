119b4ae76098332db74c03abbeab9fdbab593984
artasdaa <cellom293@gmail.com>
2025-11-05 11:32:25 +0700

    Percobaan 4

--- SearchNilai12.java
+++ SearchNilai12.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class SearchNilai12 { 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
         int jumlahNilai = sc.nextInt();
         
